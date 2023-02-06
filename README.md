66b29aba40cb77f9ed20b9a0e3137ba4f67b65f4
arazgurbanli <124072387+arazgurbanli@users.noreply.github.com>
2023-02-06 13:46:09 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Araz-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-sps8gx)
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container mt-3">
+  <div class="grid">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-succes fw-bold">App Component</p>
+        <p class="content fa fa-home text succes">sss</p>
+
+      </div>
+    </div>
+  </div>  
+</div>
+
+</template>
+
+
+<script>
+export default{}
+</script>
+
+
+<style>
+
+</style>
